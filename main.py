66f8f4e9762ca2ecef917aeda7f24cf88d93617c
NolanSpiekermeier10 <820382@mystma.org>
2022-09-13 09:41:12 -0500

    3 Functuns

---
+++ main.py
@@ -0,0 +1,12 @@
+def nameNolan():
+    print("Nolan lukas spiekermeier")
+
+def top5Letters():
+    print ("e,c,v,m,n")
+
+def fittnessGramPacer():
+    print ("The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. Students begin at the starting line. Once the test begins, the running speed starts slowly, but gets faster each minute after you hear the signal.")
+
+nameNolan()
+top5Letters()
+
